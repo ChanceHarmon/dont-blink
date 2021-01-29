4e2951b151635ddcf9250891943aa42d75a386f5
Chance Harmon <henrikillsit@gmail.com>
2021-01-28 22:25:42 -0800

    very base server is up

--- server.js
+++ server.js
@@ -1,1 +1,43 @@
 'use strict';
+
+require('dotenv').config();
+
+// Library requirements, don't forget to install them all in your terminal as you go.
+// Remeber, this is going to change as we go. Error can not find module means install the damn thing
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+// Process env declarations
+
+const PORT = process.env.PORT;
+
+
+
+//Middleware
+
+app.use(cors())
+
+
+// Routes
+
+app.get('/', homeRoute);
+
+
+// Functions
+function homeRoute(request, response) {
+  response.status(200).send('One take is all I need.');
+}
+
+
+// Errors
+
+
+// Connections
+
+app.listen(PORT, () => {
+  console.log(`Eyes wide open on ${PORT}`)
+});
+
